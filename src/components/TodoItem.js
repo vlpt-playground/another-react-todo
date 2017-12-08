c1d9a88b11cdb02782649be673697f40c013c5c8
velopert <public.velopert@gmail.com>
2017-12-09 05:26:23 +0900

    component re-render optimization

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.checked !== nextProps.checked;
+  }
+  
   render() {
     const { text, checked, id, onToggle, onRemove } = this.props;
-    
+
     return (
       <div className="todo-item" onClick={() => onToggle(id)}>
         <div className="remove" onClick={(e) => {
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.todos !== nextProps.todos;
+  }
+  
   render() {
     const { todos, onToggle, onRemove } = this.props;
     
