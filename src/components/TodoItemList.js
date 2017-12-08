3d189db4e7fff6f0ebb43e85323dac0552774168
velopert <public.velopert@gmail.com>
2017-12-09 04:59:18 +0900

    implemented functionality

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -5,11 +5,22 @@ class TodoItemList extends Component {
   render() {
     const { todos, onToggle, onRemove } = this.props;
     
+    const todoList = todos.map(
+      ({id, text, checked}) => (
+        <TodoItem
+          id={id}
+          text={text}
+          checked={checked}
+          onToggle={onToggle}
+          onRemove={onRemove}
+          key={id}
+        />
+      )
+    );
+
     return (
       <div>
-        <TodoItem text="안녕"/>
-        <TodoItem text="리액트"/>
-        <TodoItem text="반가워"/>        
+        {todoList}    
       </div>
     );
   }
